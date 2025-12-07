6bbdf50f6688f0cffca81ce286447043a5918cbe
Abimael Garcia <abimael1992g@gmail.com>
2025-12-07 13:19:02 -0600

    fix adjust page layout for responsive color changes

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,9 @@ import { RepoActivitySection } from './components/RepoActivitySection';
 import { fetchWithAuth, getCachedData, setCachedData, loadInactivityData } from '@/lib/github';
 import { ChartData, InactiveRepo, InactivityData, RepoStat } from '@/types';
 import TodoList from './components/TodoList';
+// Replace the COLORS array in your main page with:
+import { COLORS } from '@/lib/colors';
+// Remove the local COLORS array
 
 interface GitHubCommit {
     sha: string;
@@ -46,31 +49,6 @@ interface CommitData {
     author?: string;
 }
 
-
-// Constants outside component
-const COLORS = [
-    '#FF0000', // Red
-    '#FF7F00', // Orange
-    '#FFFF00', // Yellow
-    '#00FF00', // Lime
-    '#00FFFF', // Cyan
-    '#0000FF', // Blue
-    '#8B00FF', // Violet
-    '#FF1493', // Pink
-    '#808080', // Gray
-    '#000000', // Black
-    '#FFFFFF', // White
-
-    '#FF3B3F', '#FF9F1C', '#F9F871', '#1BE7FF', '#9B5DE5',
-    '#F15BB5', '#00BBF9', '#00F5D4', '#073B4C', '#FF6B6B',
-    '#FF9671', '#FFC75F', '#2EC4B6', '#845EC2', '#0081CF',
-    '#4B4453', '#D65DB1', '#FF8066', '#00C9A7', '#C34A36',
-    '#FF5E5B', '#6A0572', '#AB83A1', '#38B000', '#FFB703',
-    '#219EBC', '#8ECAE6', '#E63946', '#F77F00', '#8338EC',
-    '#3A86FF', '#FF006E', '#FB5607', '#FFBE0B', '#06D6A0',
-    '#118AB2', '#EF476F', '#FFC300', '#FFD166', '#0aa67c',
-    '#E76F51', '#2A9D8F', '#264653', '#F4A261', '#E9C46A'
-];
 
 const IGNORED_REPOS = [
     "ecommerce-fe", "college_project", "my_portfolio2", "ecommerce_be", "postList",
